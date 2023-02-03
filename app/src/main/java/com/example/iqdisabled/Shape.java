835007149b45b4ff4d3ed751a3333297b05ebc58
“Yile <yile@ualberta.ca>
2023-02-02 22:19:16 -0700

    Added Star class

--- app/src/main/java/com/example/iqdisabled/Shape.java
+++ app/src/main/java/com/example/iqdisabled/Shape.java
@@ -3,5 +3,5 @@ package com.example.iqdisabled;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "blue";
 }
--- app/src/main/java/com/example/iqdisabled/Star.java
+++ app/src/main/java/com/example/iqdisabled/Star.java
@@ -1,5 +1,4 @@
 package com.example.iqdisabled;
 
 public class Star extends Shape{
-    String color = "blue";
 }
